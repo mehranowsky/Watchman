4eb49b5c8ed3911fbb59c9eabf8c6b3017f76027
mehranowsky <mehrnowski@gmail.com>
2025-11-03 08:18:27 -0500

    added uro

--- plumber.sh
+++ plumber.sh
@@ -27,7 +27,7 @@ else
     mkdir targets
 fi
 for target in subdomains/*.txt; do
-    katana -u "$target" -jc -silent | grep -vE '\.(css|jpg|jpeg|png|svg|img|gif|mp4|flv|pdf|doc|ogv|webm|wmv|webp|mov|mp3|m4a|m4p|ppt|pptx|scss|tif|tiff|ttf|otf|woff|woff2|bmp|ico|eot|htc|swf|rtf)(\?.*)?$' | sort -u > targets/$target.txt
+    katana -u "$target" -jc -silent | grep -vE '\.(css|jpg|jpeg|png|svg|img|gif|mp4|flv|pdf|doc|ogv|webm|wmv|webp|mov|mp3|m4a|m4p|ppt|pptx|scss|tif|tiff|ttf|otf|woff|woff2|bmp|ico|eot|htc|swf|rtf)(\?.*)?$' | sort -u | uro > targets/$target.txt
 done
 
 

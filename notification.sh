2dcbfa7997b9edd9e192488a5a45a551dd18aef8
mehranowsky <mehrnowski@gmail.com>
2025-11-12 06:04:35 -0500

    notificate the progress on telegram

---
+++ notification.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Set the API token and channel ID
+API_TOKEN="API_TOKEN"
+CHANNEL_ID="CHANNEL_ID"
+
+# Set the message text
+MESSAGE="$1"
+
+# Use curl to send the message
+curl -s -X POST https://api.telegram.org/bot$API_TOKEN/sendMessage -d chat_id=$CHANNEL_ID -d text="$MESSAGE"

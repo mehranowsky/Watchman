71e3ee1d201b7d10b513fae22b4846e0c8c601ae
mehranowsky <mehrnowski@gmail.com>
2025-11-13 08:56:54 -0500

    fixed subdomain sanitizer

--- plumber.sh
+++ plumber.sh
@@ -41,7 +41,8 @@ mkdir -p "$SUB_DIR"
 
 while read -r domain; do
     [ -z "$domain" ] && continue
-    subfinder -d "$domain" -all -silent -t 5 | sort -u | dnsx -silent -t 70 | httpx -silent -t 30 > "$SUB_DIR/$domain.txt"    
+    clean_name=$(echo "$domain" | sed -E 's#^https?://##I; s#/.*$##; s/:[0-9]+$//; s/[*]//g; s/%[0-9A-Fa-f]{2}/_/g; s/^-/_/')
+    subfinder -d "$domain" -all -silent -t 5 | sort -u | dnsx -silent -t 70 | httpx -silent -t 30 > "$SUB_DIR/${clean_name}.txt"
     sleep 5
 done < h1_Wildcards.txt
 ./notification.sh "Completed subdomain enumeration and starts crawling..."
